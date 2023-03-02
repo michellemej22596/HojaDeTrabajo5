2c1c542cefdbd431b3353ac24532a4823f1cb90c
Michelle Mejía Villela <97720056+michellemej22596@users.noreply.github.com>
2023-03-01 22:49:17 -0600

    Final submit

--- hojaDeTrabajo5.py
+++ hojaDeTrabajo5.py
@@ -6,7 +6,7 @@ import random
 import simpy
 
 env = simpy.Environment()
-processes= 200 #25, 50, 100, 150, 200
+processes= 25 #25, 50, 100, 150, 200
 interval = 1.0 #10, 5, 1
 RAM = simpy.Container(env, init=100, capacity=100) #init and capacity 200
 CPU = simpy.Resource(env, capacity=1) 	  
@@ -30,18 +30,19 @@ def process(env, name, RAM, CPU, time_in_bank):
     if RAM.level>0:
         print('%7.4f %s: Un proceso ha llegado' % (arrive, name))
     else: 
+        # Ram llena
         print("La RAM se encuentra llena, por favor espere a que se desocupe")
         yield env.timeout(5)
 
     with CPU.request() as req:
         instructions = random.uniform(1, 10)
-        # Wait for the CPU or abort at the end of our tether
-        results = yield req | env.timeout(instructions)
+        # Espera
+        results = yield req | env.timeout(instructions/2)
 
         wait = env.now - arrive
 
         if req in results:
-            # We got to the CPU
+            #CPU
             print('%7.4f %s: El proceso ha esperado %6.3f' % (env.now, name, wait))
 
             tib = random.expovariate(1.0 / time_in_bank)
